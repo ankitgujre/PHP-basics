2677957311b0ba416a2a1e8fafea8c1daf376231
Ankit Gujre <ankitgujre374@gmail.com>
2026-03-07 09:54:18 +0530

    php basic structure and variables

---
+++ Variables.php
@@ -0,0 +1,16 @@
+<!-- In php variable names begins with $ sign, followed by a name -->
+
+<!------------------- Rules ----------------------->
+<!----------------------
+    * variable name starts from $ sign
+    * variable name only starts with a letters or (_) underscore.
+    * variable name cannot start with a number
+    *  It is case sensitive num lowercase and NUM uppercase are different variables
+    * do not use reserved keywords
+------------->
+<?php
+	$rollNo = 123;
+	$student = "Ankit";
+	echo $rollNo;
+	echo $student
+?>
