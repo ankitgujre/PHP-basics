ef6c58d147298f4625190842d67ddf53b1333944
Ankit Gujre <ankitgujre374@gmail.com>
2026-03-08 09:22:29 +0530

    echo statement

---
+++ ecoStatement.php
@@ -0,0 +1,18 @@
+<!-- eco statement is used to output the data to the screen or printing
+text on screen -->
+
+<?php
+    echo "Hello </br>";
+    echo 'hello </br>';
+    echo 25.32;
+    echo ('</br>hiiiiiii</br>');
+    echo 'hello', 'world<br>';
+
+    $value1 = 10;
+    $value2 = 1000;
+
+    echo $value1;
+    echo "Value 1 = ",  $value1 ,"<br>";
+
+    echo "<b> Bold </b>";
+?>
